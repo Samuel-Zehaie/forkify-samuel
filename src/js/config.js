d78f67520fe0aeb181e575b10161b9b1511fc400
Samuel-Zehaie <samuelzehaie@gmail.com>
2024-07-18 23:00:31 -0700

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`
+export const TIMEOUT_SEC = 10
+export const RES_PER_PAGE = 10
+export const KEY = '3dd2440c-de88-43c2-b8e7-bd3e5f78eda4';
+export const MODAL_CLOSE_SEC = 2.5;
